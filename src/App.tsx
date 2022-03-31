2c9b470c8b35a9b599e0b16fa670ca83f714e5d5
jonnelrodjiptolosa <jonnelrodji.tolosa@hcl.com>
2022-04-01 02:02:08 +0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
+import { TextField, Button, Box, createStyles, Theme, InputLabel, MenuItem, Select, FormControl } from '@mui/material';
+import { makeStyles } from '@mui/material/styles';
+
 import './App.css';
 
-function App() {
+
+type ParkingSlotSize = 'SP' | 'MP' | 'LP';
+interface IParkingSlot {
+  entryPoint: number,
+  slot: number,
+}
+
+const App = () => {
+  // Row = entry point
+  // Column = slot
+  const [entryPoint, setEntryPoint] = React.useState(0);
+  const [slot, setSlot] = React.useState(0);
+  const [isClicked, setIsClicked] = React.useState(false);
+
+  const handleSetEntryPoint = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setEntryPoint(Number(e.target.value));
+  };
+
+  const handleSetSlot = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setSlot(Number(e.target.value));
+  };
+
+  const ParkingSlot: React.FC<IParkingSlot> = (props: IParkingSlot) => {
+    return (
+      <Box className='Parking-Slot-Box'>
+        <p>EP: {props.entryPoint}</p>
+        <p>SLOT: {props.slot}</p>
+        <InputLabel id="demo-simple-select-standard-label">Size</InputLabel>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <Select
+            // value={age}
+            // onChange={handleChange}
+            label="Size"
+          >
+            <MenuItem value="SP">SP</MenuItem>
+            <MenuItem value="MP">MP</MenuItem>
+            <MenuItem value="LP">LP</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    )
+  };
+
+  const ParkingArea = () => {
+    // TODO: add validation - entryPoint should be greater than or equal 3
+    let list = [];
+    for (let i = 1; i <= entryPoint; i++) {
+      for (let j = 1; j <= slot; j++) {
+        list.push(<ParkingSlot entryPoint={i} slot={j} />)
+      }
+    }
+    return (
+      <>
+        <Box display="flex" flexWrap="wrap" justifyContent="center" style={{ gap: '80px', marginTop: '100px' }}>
+          {list}
+        </Box>
+      </>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Parking System</h1>
+      <Box display="flex" justifyContent="center" style={{ gap: '10px' }}>
+        <TextField
+          placeholder="Enter number of entry points"
+          onChange={(e) => { return handleSetEntryPoint(e) }}
+          value={entryPoint}
+        />
+        <TextField
+          placeholder="Enter number of parking slot"
+          onChange={(e) => { return handleSetSlot(e) }}
+          value={slot}
+        />
+        <Button
+          variant="contained"
+          className=''
+          onClick={() => { return setIsClicked(true); }}
+        >Submit</Button>
+      </Box>
+      {isClicked ? ParkingArea() : ''}
     </div>
   );
 }
